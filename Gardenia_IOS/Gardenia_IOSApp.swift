700e0a5ead43909933edd4e79a46e00bd0edda86
nmk098 <nkuryumov@gmail.com>
2023-08-03 11:59:09 +0300

    Initial Commit

---
+++ Gardenia_IOS/Gardenia_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gardenia_IOSApp.swift
+//  Gardenia_IOS
+//
+//  Created by Никита Курюмов on 03.08.23.
+//
+
+import SwiftUI
+
+@main
+struct Gardenia_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
